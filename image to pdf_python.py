e6983fc3eddd85d97dfb05326d60bacefb01f7f2
LAshleyyy <al2024059@gmail.com>
2024-09-04 11:42:50 +0800

    Update image to pdf_python.py
    save as a4 size already now, this done

--- image to pdf_python.py
+++ image to pdf_python.py
@@ -12,6 +12,19 @@ pdf_path = "C:\\Users\\user\\Downloads\\quokka_pdf.pdf"
 # opening image
 image = Image.open(img_path)
 
+
+
+# setting A4 page dimensions in points (1 point = 1/72 inch)
+a4_width = img2pdf.mm_to_pt(210)  # A4 width in points
+a4_height = img2pdf.mm_to_pt(297)  # A4 height in points
+a4inpt = (a4_width, a4_height)
+
+# converting image to PDF with A4 layout, resizing image to A4 dimensions
+pdf_bytes = img2pdf.convert(image.filename, 
+                            layout_fun=img2pdf.get_layout_fun(a4inpt))
+
+
+
 # converting into chunks using img2pdf
 pdf_bytes = img2pdf.convert(image.filename)
 
